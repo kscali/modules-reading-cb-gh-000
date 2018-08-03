c4cd3ca31ab9cb197314eb89a2a3d056588c14df
Koy Saeteurn <kosatun@aol.com>
2018-08-03 01:25:02 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,4 +5,8 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  def initialize(name)
+    @name = name 
+  end 
 end
